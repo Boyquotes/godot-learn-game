0672f2a9c513e5b5ddee4140803b7244c8ee2a59
Alexandr Seliukov <43579724+asexsela@users.noreply.github.com>
2023-06-07 07:15:20 +0200

    init

---
+++ Characters/player_cat.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 100
+@export var starting_direction: Vector2 = Vector2(0, 1)
+
+@onready var anomation_tree = $AnimationTree
+@onready var state_machine = anomation_tree.get("parameters/playback")
+
+
+func _ready():
+	update_animation_parameters(starting_direction)
+
+
+func _physics_process(delta):
+	# get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")		
+	)
+	
+	update_animation_parameters(input_direction)
+	
+	# update velocity
+	velocity = input_direction * move_speed
+	
+	# move and slice functions uses velocity of character body to move character on map
+	move_and_slide()
+	
+	pick_new_state()
+	
+func update_animation_parameters(move_input: Vector2):
+	if (move_input != Vector2.ZERO):
+		anomation_tree.set("parameters/Idle/blend_position", move_input)
+		anomation_tree.set("parameters/Walk/blend_position", move_input)
+		
+		
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
